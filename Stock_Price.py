d77f40a57e71ef3bfbebdc48f8177a2e9ac09d66
aalejo92 <76714792+aalejo92@users.noreply.github.com>
2021-03-23 19:46:57 -0400

    Update Stock_Price.py
    03-23-21 - Reformatted and added notes to code

--- Stock_Price.py
+++ Stock_Price.py
@@ -1,21 +1,19 @@
 import yfinance as yf
-# import pandas as pd
 import streamlit as st
 
 st.write("""
-# Stock Price App
+# Simple Historical Stock Data
 
-Shown are the stock closing price and volume of Google!
+Below are the stock closing price and volume of Apple!
 """)
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 # define the ticker symbol
-tickerSymbol = 'GOOGL'
+tickerSymbol = 'AAPL'
 # get dat on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 # get the historical prices for this ticker
-tickerDF = tickerData.history(period='1d', start='2010-5-31',end='2020-5-31')
-
+tickerDF = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.line_chart(tickerDF.Close)
 st.line_chart(tickerDF.Volume)
-
-# (what I use to run on PyCharm) streamlit run /Users/A3X/PycharmProjects/pythonPractice/Stock_Price.py [ARGUMENTS]
